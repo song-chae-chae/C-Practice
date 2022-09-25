2fd03c0be8cad5176356bbb5d80e6ed04f7cd085
chaechae <songchaechaes@gmail.com>
2022-09-25 19:25:14 +0900

    add Chapter1 practice

---
+++ Chapter1/Challenge-1-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("이창현\n경기도 수원시 영통구 매탄동\n010-3333-xxxx\n");
+
+	printf("------------------------------------\n");
+
+	printf("이창현\n");
+	printf("경기도 수원시 영통구 매탄동\n");
+	printf("010-3333-xxxx\n");
+	return 0;
+}
