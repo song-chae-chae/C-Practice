45aef2b9c48d8cd624d13d71dd9663a5ad9ce98b
chaechae <songchaechaes@gmail.com>
2022-10-07 22:48:45 +0900

    add Chapter11 practices

---
+++ Chapter11/Practice-11-10.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void main() {
+	// NULL 포인터 : 아무것도 가리키지 않는 포인터 - 포인터 변수에 0을 넣으면 아무것도 가리키지 않는다고 간주하고 NULL 포인터라고 명명
+	// 함수의 동작 에러 체크 용도로 주로 사용
+	// 0을 넣으면 아무것도 가리키지 않는다고 말하지만 엄밀히 말하면 0번지를 가리키고 - 0번지는 시스템 영역임
+
+	char str[] = "Love";
+	char* p;
+
+	p = strchr(str, 'v'); // strchr : str에서 v를 검색하여 위치 주소값을 리턴하는 함수
+
+	if (p != NULL) {
+		*p = 'b'; // strchr 함수의 리턴값인 주소값을 참조해서 실제 값을 b로 변경해버림
+		printf("변경 결과 : %s\n", str);
+
+	}
+	else {
+		printf("NULL을 리턴 \n");
+	}
+
+}
+
+int Add(int a, int b) {
+	return a + b;
+}
+
+int Min(int a, int b) {
+	return a - b;
+}
---
+++ Chapter11/Practice-11-9.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+
+int Add(int a, int b);
+int Min(int a, int b);
+
+void main() {
+	// 함수 포인터 - 함수도 엄밀하게 생각해보면 포인터
+	// 선언, 정의, 호출 - 함수를 호출한다는 의미는 함수가 정의되어 있는 메모리 주소를 찾아서 호출한다는 의미
+	// 함수의 내용은 내가 호출하고자 하는 코드의 위치에 있는 것이 아니고, 어딘가에 정의되어 있고, 필요할 때 그 위치를 참조 하는 것
+	// 왜 함수 포인터를 굳이 사용?
+	// 코드를 컴파일하게 되면 코드의 변수, 함수들은 컴파일 타임에 메모리의 크기와 위치 결정됨
+	// 메모리 위치는 이때 정해지고 함수의 경우 호출 시 매번 같은 주소 참조 - 정적 바인딩
+	// 컴파일 타임이 아닌 프로그램이 실행 시 메모리 할당되는 경우(런타임) - 동적 바인딩
+	// 프로그램의 확장성과 유용성을 위해 사용 ex)플러그인에 새로운 함수 추가될때마다 다시 컴파일하는 비효율적인 상황 해결
+
+	// 함수 포인터를 통해서 동적으로 함수 호출하는 예제
+	int a, b, sel, result;
+	int(*fPtr)(int a, int b); // Add, Min 함수를 참조하기 위한 두개의 정수형 매개변수를 갖는 함수 포인터 선언
+
+	while (1) {
+		printf("다음 중 선택하시오 (1.덧셈 2.뺄셈 3.종료) : ");
+		scanf_s("%d", &sel);
+
+		switch (sel) {
+		case 1:
+			fPtr = Add; // fPtr에 Add 함수의 주소값 대입
+			break;
+		case 2:
+			fPtr = Min;
+			break;
+		case 3:
+			return;
+		default:
+			fPtr = Add;
+			break;
+		}
+
+		printf("두 정수를 입력하시오 : ");
+		scanf_s("%d%d", &a, &b);
+
+		result = fPtr(a, b);
+		printf("결과 : %d\n", result);
+	}
+}
+
+int Add(int a, int b) {
+	return a + b;
+}
+
+int Min(int a, int b) {
+	return a - b;
+}
