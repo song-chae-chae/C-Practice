a0d5070bfe0934b10f992ed3b4fa4f94c7583d02
chaechae <songchaechaes@gmail.com>
2022-10-08 22:29:55 +0900

    add Chapter12 practices

---
+++ Chapter12/Practice-12-1.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+void main() {
+	char str[] = "have a nice day";
+	puts(str); // 개행 문자 쓰지 않고도 개행됨
+
+	fputs(str, stdout);
+	fputs("\n", stdout); // 개행문자 써줘야 개행됨
+}
---
+++ Chapter12/Practice-12-2.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+void main() {
+	char str[10];
+
+	fputs("gets 문자열 입력 : ", stdout);
+	gets(str); // 데이터 입력에 대한 메모리 오버플로우 발생 문제 있음
+	puts(str);
+
+	fputs("fgets 문자열 입력 : ", stdout);
+	fgets(str, sizeof(str), stdin); // 배열의 할당 크기만큼만 입력 가능 (오버플로우 발생 x)
+	puts(str);
+}
