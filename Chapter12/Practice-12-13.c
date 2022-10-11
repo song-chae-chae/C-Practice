7c9a2b912e4430e6fb00566ba0d494a9232e16bf
chaechae <songchaechaes@gmail.com>
2022-10-11 22:08:04 +0900

    add Chapter12 practice

---
+++ Chapter12/Practice-12-13.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void main() {
+	char str[] = "applepie";
+	int count = 0;
+	char* ptr;
+	char* rptr;
+	int i;
+
+	ptr = strchr(str, 'e');
+	rptr = strchr(str, 'e');
+
+	if (ptr == NULL) {
+		printf("문자 'e'를 찾을 수 없습니다.");
+	}
+	else {
+		printf("strchar : 문자 'e'를 %d번째 위치에서 찾았습니다.\n", (ptr - str) + 1);
+		printf("strchar : 문자 'e'를 %d번째 위치에서 찾았습니다.\n", (rptr - str) + 1);
+	}
+}
