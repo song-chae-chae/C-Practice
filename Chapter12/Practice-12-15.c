ae9b5c3fbe9fce8922b4b0a4091397b6a1e72613
chaechae <songchaechaes@gmail.com>
2022-10-12 23:06:10 +0900

    add Chapter12 practices

---
+++ Chapter12/Practice-12-15.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void main() {
+	char string[] = "f(x)=a+b*c%d-f";
+	char strSeparate[] = "%=+-*";
+	char* strPos = string;
+
+	do {
+		strPos = strpbrk(strPos, strSeparate);
+
+		if (strPos != NULL) {
+			printf("%s\n", strPos);
+			++strPos;
+		}
+	} while (strPos != NULL);
+}
